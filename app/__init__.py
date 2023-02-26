6273c95001e2caa0ac70aae6201e0cb86d7d4c57
Javier Palacios <javiplx@gmail.com>
2023-02-26 15:11:09 +0100

    Fix return from root page

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route('/', defaults={"path": "/"})
 @app.route('/<path:path>')
 def index(path):
-   return f"Hello world!<BR />(from {path})"
+   response = ["Hello world!"]
+   if path != "/":
+      response.append(f"(from {path})")
+   return "<BR />".join(response)
 
 if __name__ == "__main__":
     app.run()
